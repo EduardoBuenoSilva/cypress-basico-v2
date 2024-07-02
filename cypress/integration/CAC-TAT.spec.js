7b8f0858b1c749e829dd081468bd97ab588244e4
Eduardo Bueno <eduardobuenosilva@hotmail.com>
2024-07-02 12:02:34 -0300

    Seção - 4 ao 6

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,5 @@
 /// <reference types="Cypress" />
 
-
 describe('Central de Atendimento ao Cliente TAT', function() {
     beforeEach(function() { // before each foi inserido para que toda vez que for rodar o teste, leia primeiro a pagina, antes de cada teste vai executar o comando.
         cy.visit('./src/index.html') // visita a pagina
@@ -16,7 +15,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         
         cy.get('#firstName').type('Eduardo','delay')
         cy.get('#lastName').type('Bueno')
-        cy.get('#email').type('eduardobueno08@gmail.com')
+        cy.get('#email').type('teste@teste.com')
         cy.get('#open-text-area').type(longText, {delay: 0}) // delay diminui o tempo de velocidade 
         cy.get('button[type="submit"]').click()
 
@@ -27,7 +26,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
         cy.get('#firstName').type('Eduardo','delay')
         cy.get('#lastName').type('Bueno')
-        cy.get('#email').type('eduardobueno08@gmail,com')
+        cy.get('#email').type('teste@teste,com')
         cy.get('#open-text-area').type('Teste')
         cy.get('button[type="submit"]').click()
 
@@ -45,7 +44,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
         cy.get('#firstName').type('Eduardo')
         cy.get('#lastName').type('Bueno')
-        cy.get('#email').type('eduardobueno08@gmail.com')
+        cy.get('#email').type('teste@teste.com')
         cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('Teste')
         cy.get('button[type="submit"]').click()
@@ -66,12 +65,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .clear()
             .should('have.value','')
         cy.get('#email')
-            .type('eduardobueno08@gmail.com')
-            .should('have.value', 'eduardobueno08@gmail.com')
+            .type('teste@teste.com')
+            .should('have.value', 'teste@teste.com')
             .clear()
             .should('have.value','')
         cy.get('#phone')
-            .type('13974237518')
+            .type('123456789')
             .should('have.value', '123456789')
             .clear()
             .should('have.value','')
@@ -87,7 +86,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     //exercicio 7 - Criar Comandos customizados
 
-    it.only('envia o formuário com sucesso usando um comando customizado', function() {
+    it('envia o formuário com sucesso usando um comando customizado', function() {
         cy.fillMandatoryFieldsAndSubmit()
 
         cy.get('.success').should('be.visible')
@@ -98,11 +97,78 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('Contains', function() {
         cy.get('#firstName').type('Eduardo','delay')
         cy.get('#lastName').type('Bueno')
-        cy.get('#email').type('eduardobueno08@gmail.com')
+        cy.get('#email').type('teste@teste.com')
         cy.get('#open-text-area').type('Teste','Enviar') //escrever e procura o nome do botão e clica
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
     })
 
+//------------------------------ Seção 4 ------------------------------
+
+
+    //Exercicio - Text
+    it('Seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product') // Utilizar o ID para ser mais específico
+          .select('YouTube')
+          .should('have.value', 'youtube')
+    })
+
+    //Exercicio 1 - Value
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product')
+        .select('mentoria')
+        .should('have.value','mentoria')
+    })
+    
+    //Exercicio 2 - Indice
+    it('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product')
+        .select(1)
+        .should('have.value','blog')
+    })
+    
+//------------------------------ Seção 5 ------------------------------
+
+    //Exercicio - Usar .check () para questões de semantica
+    it('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"][value="feedback"]')
+        .check()
+        .should('have.value','feedback')
+    })
+
+    //Exercicio Extra
+    it('marca cada tipo de atendimento', function() {
+        cy.get('input[type="radio"]')  // seleciona todos
+        .should('have.length', 3)      // faz uma contagem de quantos tem
+        .each(function($radio) {        //encadeia o each para passar por cada um deles, no qual criamos a função que recebe cada um dos elementos
+            cy.wrap($radio).check()    // wrap para empacotar os elementos e pode mandar comandos
+            cy.wrap($radio).should('be.checked')
+        })
+    })
+
+//------------------------------ Seção 6 ------------------------------
+
+    // Exercicio 
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+        cy.get('input[type="checkbox"]')
+        .check()
+        .should('be.checked')
+        .last()
+        .uncheck()
+        .should('not.be.checked')
+    })
+
+    // Exercicio Extra
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName').type('Eduardo')
+        cy.get('#lastName').type('Bueno')
+        cy.get('#email').type('teste@teste.com')
+        cy.get('#phone-checkbox').check()
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+    
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
     cy.get('#firstName').type('Eduardo','delay')
         cy.get('#lastName').type('Bueno')
-        cy.get('#email').type('eduardobueno08@gmail.com')
+        cy.get('#email').type('teste@teste.com')
         cy.get('#open-text-area').type('Teste')
         cy.get('button[type="submit"]').click()
 })
