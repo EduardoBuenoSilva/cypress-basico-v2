d3ee1ded3774eecfe148180c53597d878ad7d5cb
EduardoBuenoSilva <78116094+EduardoBuenoSilva@users.noreply.github.com>
2024-07-03 19:39:21 -0300

    Seção 8

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -208,4 +208,31 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
  //------------------------------ Seção 8 ------------------------------   
  
+    // Exercicio
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+        cy.get('#privacy a')
+        .should('have.attr', 'target', '_blank')
+    })
+
+    // Exercicio extra 1
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function () {
+        cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click()
+
+        cy.contains('Talking About Testing').should('be.visible')
+    })
+
+    // Exercicio extra 2
+
+    it('testa a página da política de privacidade de forma independente', function () {
+        cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click()
+
+        cy.contains('Talking About Testing').should('be.visible')
+    })
+
+    
+
 })
