ef6411648e9dd778e5e6f2aa0a159c43bc1c5203
EduardoBuenoSilva <78116094+EduardoBuenoSilva@users.noreply.github.com>
2024-12-28 17:10:25 -0300

    Seção 4 ao 8

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -85,4 +85,86 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible')
     })
 
+    it('seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product').select('YouTube')
+        .should('have.value','youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product').select('mentoria')
+        .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product').select(1)
+        .should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"]').check('feedback')
+        .should('have.value','feedback')
+    })
+
+    it('marca cada tipo de atendimento', function() {
+        cy.get('input[type="radio"]')
+        .should('have.length', 3)
+        .each(function($radio) {
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+        })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+        cy.get('input[type="checkbox"]')
+        .check()
+        .last()
+        .uncheck()
+        .should('not.be.checked')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName').type('Eduardo')
+        cy.get('#lastName').type('Bueno')
+        cy.get('#email').type('eduardoteste@hotmail.com')
+        cy.get('#phone-checkbox').check()
+        cy.get('#open-text-area').should('be.visible').type('Teste')
+        cy.contains('button', 'Enviar').click()
+        
+        cy.get('.error').should('be.visible')
+    })
+
+    it('seleciona um arquivo da pasta fixtures', function() {
+        cy.get('input[type="file"]#file-upload')
+        .selectFile('cypress/fixtures/example.json')
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function() {
+        cy.get('input[type="file"]#file-upload')
+        .selectFile('cypress/fixtures/example.json', {action: 'drag-drop'})
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+        .selectFile('@sampleFile')
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+        cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click()
+    })
 })
